542774dab0008733b79e94eef6748c22f734462a
Theomat <theomatricon@gmail.com>
2024-12-23 11:31:19 +0100

    reduced maximum number of iterations

--- examples/pbe/karel/karel.py
+++ examples/pbe/karel/karel.py
@@ -150,7 +150,7 @@ __syntax = auto_type(
 
 def __while(w: KarelWorld, c, s) -> KarelWorld:
     n = 0
-    while c(w) and n < 10000:
+    while c(w) and n < 1000:
         w = s(w)
         n += 1
     return w
