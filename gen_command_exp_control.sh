2609e236893a36aa900c5ce3e29087ef3fb753ec
Theomat <theomatricon@gmail.com>
2025-01-10 16:18:23 +0100

    avoir re-generating dfta

--- gen_command_exp_control.sh
+++ gen_command_exp_control.sh
@@ -6,9 +6,16 @@ DFTA_SUFFIX="_dfta_filter_control"
 SEEDS="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"
 
 function gen_dfta(){
-    python examples/rl/dsl_equation_generator.py -s $1 --n 1000 > /dev/null
-    python examples/rl/equivalence_classes_to_filter.py equivalent_classes_control.json > /dev/null
-    mv dfta_filter_control.py automatic_dfta_filter_control_$1.py
+    dst="automatic_dfta_filter_control_$1.py"
+    json="equivalent_classes_control_$1.json"
+    if [ ! -f "$json" ]; then
+        python examples/rl/dsl_equation_generator.py -s $1 --n 1000 > /dev/null
+        mv equivalent_classes_control.json $json
+    fi
+    if [ ! -f "$dst" ]; then
+        python examples/rl/equivalence_classes_to_filter.py $json > /dev/null
+        mv dfta_filter_control.py $dst
+    fi
 }
 
 function run_exp(){
