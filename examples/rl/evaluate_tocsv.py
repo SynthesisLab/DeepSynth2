7d6836b70b5aef718eeabf688c887d946f9eb890
Théo Matricon <theomatricon@gmail.com>
2025-04-14 10:11:53 +0200

    fixed partial loading and added anytime saving

--- examples/rl/evaluate_tocsv.py
+++ examples/rl/evaluate_tocsv.py
@@ -1,18 +1,20 @@
-from typing import Callable, List, Tuple
+from typing import Callable, List
 import gymnasium as gym
+from examples.rl.program_evaluator import ProgramEvaluator
 from synth.semantic.evaluator import Evaluator
 from synth.syntax.program import Program
-import numpy as np
 import pandas as pd
 import os
+import atexit
+
 
 def evaluate_programs_to_csv(
-    programs: List[Program], 
-    env_factory: Callable[[], gym.Env], 
-    evaluator: Evaluator, 
-    num_seeds: int, 
-    csv_file: str = "program_evaluation.csv"
-    ):
+    programs: List[Program],
+    env_factory: Callable[[], gym.Env],
+    evaluator: Evaluator,
+    num_seeds: int,
+    csv_file: str = "program_evaluation.csv",
+):
     """
     Evaluates a list of programs on an environment for multiple seeds and saves the results to a CSV.
 
@@ -26,32 +28,40 @@ def evaluate_programs_to_csv(
     program_evaluator = ProgramEvaluator(env_factory, evaluator)
     results = {}
 
+    def save():
+        data = []
+        for program, rewards in results.items():
+            row = [str(program)] + rewards
+            data.append(row)
+        columns = ["Program"] + [f"reward{i + 1}" for i in range(num_seeds)]
+        df = pd.DataFrame(data, columns=columns)
+        df.to_csv(csv_file, index=False)
+
     if os.path.exists(csv_file):
         df = pd.read_csv(csv_file)
         for index, row in df.iterrows():
-            program_str = row['Program']
-            rewards = row[1:].tolist()
+            program_str = row["Program"]
+            rewards = [el for el in row[1:].tolist() if str(el) != "None"]
             program = next((p for p in programs if str(p) == program_str), None)
             if program:
                 results[program] = rewards
+            else:
+                print("failed parsing:", program)
 
     for program in programs:
         if program not in results:
-            results[program] = [0.0] * num_seeds
+            results[program] = [None] * num_seeds
+        elif len(results[program]) < num_seeds:
+            results[program] += [None] * (num_seeds - len(results[program]))
 
+    atexit.register(save)
     for program in programs:
         for seed in range(num_seeds):
-            env = env_factory()
-            env.reset(seed=seed)
-            program_evaluator.cache[program.hash] = (env, [])
-            program_evaluator.eval(program, 1)
-            results[program][seed] = program_evaluator.returns(program)[-1]
-        
-    data = []
-    for program, rewards in results.items():
-        row = [str(program)] + rewards
-        data.append(row)
-    
-    columns = ["Program"] + [f"reward{i+1}" for i in range(num_seeds)]
-    df = pd.DataFrame(data, columns=columns)
-    df.to_csv(csv_file, index=False)
+            if results[program][seed] is None:
+                env = env_factory()
+                env.reset(seed=seed)
+                program_evaluator.cache[program.hash] = (env, [])
+                program_evaluator.eval(program, 1)
+                results[program][seed] = program_evaluator.returns(program)[-1]
+
+    atexit.unregister(save)
