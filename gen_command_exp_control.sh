5dc6692877ff8bc8169be7f4458db60e78865f96
Théo Matricon <theomatricon@gmail.com>
2025-04-14 11:54:11 +0200

    updated with targets

--- gen_command_exp_control.sh
+++ gen_command_exp_control.sh
@@ -2,31 +2,27 @@
 ENV="$1"
 filename=$(basename -- "$ENV")
 NAME="${filename%.*}"
-DFTA_SUFFIX="_dfta_filter_control"
 SEEDS="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"
-
-function gen_dfta(){
-    dst="automatic_dfta_filter_control_$1.py"
-    json="equivalent_classes_control_$1.json"
-    if [ ! -f "$json" ]; then
-        python examples/rl/dsl_equation_generator.py -s $1 --n 1000 > /dev/null
-        mv equivalent_classes_control.json $json
-    fi
-    if [ ! -f "$dst" ]; then
-        python examples/rl/equivalence_classes_to_filter.py $json > /dev/null
-        mv dfta_filter_control.py $dst
-    fi
-}
+SOLVERS="sampling none"
+OPTI="mab none"
 
 function run_exp(){
     seed=$1
     dfta=$2
-    filename="$NAME/${dfta}_$seed.csv"
+    solver=$3
+    opti=$4
+    filename="$NAME/${dfta}_$seed_${solver}_${opti}.csv"
+    # echo "file:$filename"
     if [ ! -f "$filename" ]; then
         arg="--automaton 'control.txt'"
-        # arg="--filter ${dfta}$DFTA_SUFFIX"
-        if [[ -z "$dfta" ]]; then
+        if [[ -z $dfta ]]; then
             arg=""
+        fi
+        if [[ ! $solver == "none" ]]; then
+            arg="$arg --$solver"
+        fi
+        if [[ ! $opti == "none" ]]; then
+            arg="$arg --$opti"
         fi
         echo "python examples/rl/solve.py --seed $seed -o $filename $arg @$ENV"
     fi
@@ -38,7 +34,10 @@ fi
 
 for SEED in $SEEDS
 do
-    # gen_dfta $SEED
-    run_exp $SEED "automatic"
-    run_exp $SEED
+    for solver in $SOLVERS; do
+        for opti in $OPTI; do
+            run_exp $SEED "automatic" $solver $opti
+            run_exp $SEED "" $solver $opti
+        done
+    done
 done
