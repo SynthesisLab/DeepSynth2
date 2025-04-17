a0a5d98ac56432a1de596b248710be9c80e33137
Théo Matricon <theomatricon@gmail.com>
2025-04-17 14:52:16 +0200

    fixed script

--- gen_eval_all_progs.sh
+++ gen_eval_all_progs.sh
@@ -3,8 +3,8 @@ ENV="$1"
 SEEDS="1"
 filename=$(basename -- "$ENV")
 NAME="${filename%.*}"
-BASIC="../grape/control_basic15.grape"
-FILTER="../grape/inf_control9.grape"
+BASIC="./control_basic15.grape"
+FILTER="./inf_control9.grape"
 SIZE=10
 
 if [ ! -d "./$NAME" ]; then
@@ -15,8 +15,8 @@ for SEED in $SEEDS
 do
     basic_progs="${NAME}_basic_seed$seed.txt"
     filter_progs="${NAME}_filter_seed$seed.txt"
-    echo "python examples/rl/gen_programs.py @$filename --size $SIZE --automaton $BASIC --seed $seed -o $basic_progs"
-    echo "python examples/rl/gen_programs.py @$filename --size $SIZE --automaton $FILTER --seed $seed -o $filter_progs"
-    echo "python examples/rl/eval_all.py @$filename --seed $SEED -f $basic_progs -o c${NAME}_seed${seed}_basic.csv"
-    echo "python examples/rl/eval_all.py @$filename --seed $SEED -f $filter_progs -o c${NAME}_seed${seed}_filter.csv"
+    echo "python examples/rl/gen_programs.py @$ENV --size $SIZE --automaton $BASIC --seed $seed -o $basic_progs"
+    echo "python examples/rl/gen_programs.py @$ENV --size $SIZE --automaton $FILTER --seed $seed -o $filter_progs"
+    echo "python examples/rl/eval_all.py @$ENV --seed $SEED -f $basic_progs -o c${NAME}_seed${seed}_basic.csv"
+    echo "python examples/rl/eval_all.py @$ENV --seed $SEED -f $filter_progs -o c${NAME}_seed${seed}_filter.csv"
 done
