b6c2fe51d3559e55a463e3592bb2b8de2a7c268e
Théo Matricon <theomatricon@gmail.com>
2025-05-07 11:57:29 +0200

    added grammar with constants

--- gen_eval_all_progs.sh
+++ gen_eval_all_progs.sh
@@ -4,6 +4,8 @@ SEEDS="1"
 filename=$(basename -- "$ENV")
 NAME="${filename%.*}"
 BASIC="./grammars/basic.grape"
+BASIC_CST="./grammars/basic_with_csts.grape"
+FILTER_CST="./grammars/filter_with_csts.grape"
 FILTER="./grammars/filter.grape"
 SIZE=10
 if [[ $NAME == "lunar_lander" || $NAME == "acrobot" ]]; then
@@ -17,9 +19,12 @@ fi
 for SEED in $SEEDS
 do
     basic_progs="./$NAME/progs_${NAME}_basic_seed$SEED.txt"
+    basic_progs_cst="./$NAME/progs_${NAME}_basic_with_csts_seed$SEED.txt"
     filter_progs="./$NAME/progs_${NAME}_filter_seed$SEED.txt"
     echo "python examples/rl/gen_programs.py @$ENV --size $SIZE --automaton $BASIC --seed $SEED -o $basic_progs"
+    echo "python examples/rl/gen_programs.py @$ENV --size $SIZE --automaton $BASIC_CST --seed $SEED -o $basic_progs_cst"
     echo "python examples/rl/gen_programs.py @$ENV --size $SIZE --automaton $FILTER --seed $SEED -o $filter_progs"
     echo "python examples/rl/eval_all.py @$ENV --seed $SEED -f $basic_progs -o ./$NAME/progs_eval_${NAME}_seed${SEED}_basic.csv"
+    echo "python examples/rl/eval_all.py @$ENV --seed $SEED -f $basic_progs_cst -o ./$NAME/progs_eval_${NAME}_seed${SEED}_basic_with_csts.csv"
     echo "python examples/rl/eval_all.py @$ENV --seed $SEED -f $filter_progs -o ./$NAME/progs_eval_${NAME}_seed${SEED}_filter.csv --warm ./$NAME/progs_eval_${NAME}_seed${SEED}_basic.csv"
 done
