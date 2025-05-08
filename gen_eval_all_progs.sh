167930c6c4462c3ce27dfe01b71bd4b049eff119
Théo Matricon <theomatricon@gmail.com>
2025-05-08 08:38:48 +0200

    add warm-up

--- gen_eval_all_progs.sh
+++ gen_eval_all_progs.sh
@@ -25,6 +25,6 @@ do
     echo "python examples/rl/gen_programs.py @$ENV --size $SIZE --automaton $BASIC_CST --seed $SEED -o $basic_progs_cst"
     echo "python examples/rl/gen_programs.py @$ENV --size $SIZE --automaton $FILTER --seed $SEED -o $filter_progs"
     echo "python examples/rl/eval_all.py @$ENV --seed $SEED -f $basic_progs -o ./$NAME/progs_eval_${NAME}_seed${SEED}_basic.csv"
-    echo "python examples/rl/eval_all.py @$ENV --seed $SEED -f $basic_progs_cst -o ./$NAME/progs_eval_${NAME}_seed${SEED}_basic_with_csts.csv"
+    echo "python examples/rl/eval_all.py @$ENV --seed $SEED -f $basic_progs_cst -o ./$NAME/progs_eval_${NAME}_seed${SEED}_basic_with_csts.csv --warm ./$NAME/progs_eval_${NAME}_seed${SEED}_basic.csv"
     echo "python examples/rl/eval_all.py @$ENV --seed $SEED -f $filter_progs -o ./$NAME/progs_eval_${NAME}_seed${SEED}_filter.csv --warm ./$NAME/progs_eval_${NAME}_seed${SEED}_basic.csv"
 done
